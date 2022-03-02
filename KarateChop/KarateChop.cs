f0135b5718a42f6ed0a6f73562a3a817959573e4
Jesper Jensen <jesperjensen@deltek.com>
2022-03-02 20:45:23 +0100

    feat: implemented array split and position check, 6 test failing

--- KarateChop/KarateChop.cs
+++ KarateChop/KarateChop.cs
@@ -1,9 +1,57 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace KarateChop;
 
 public class KarateChop
 {
     public int ChopChop(int chopValue, int[] array)
     {
-        return 0;
+        if (array == Array.Empty<int>())
+        {
+            return -1;
+        }
+
+        if (array.Length <= 1) return checkForChopValue(chopValue, array);
+        
+        var currentArray = splitArray(chopValue, array);
+        
+        if (currentArray.Length != 1) ChopChop(chopValue, currentArray);
+        
+        return checkForChopValue(chopValue, currentArray);
+    }
+
+    
+    private int checkForChopValue(int chopValue, int[] array)
+    {
+        return chopValue == array[0] ? Array.IndexOf(array, chopValue) : -1;
+    }
+
+    private int[] splitArray(int chopValue, int[] array)
+    {
+        var lowBounds = array.Length / 2;
+        var highBounds = array.Length % 2 == 0 ? array.Length / 2 : array.Length / 2 + 1;
+
+        var midpoint = array.Length % 2 == 0 ? 0: (array.Length / 2 + 1);
+
+        if (array.Length % 2 != 0 && array.Length > 1)
+        {
+            if(array[midpoint] == chopValue)
+                return new [] {array[midpoint]};
+        }
+            
+            
+        
+        var lowSplit = new int[lowBounds];
+        var highSplit = new int[array.Length - highBounds];
+        
+        Array.Copy(array, 0, lowSplit, 0, lowBounds);
+        Array.Copy(array, highBounds, highSplit, 0, array.Length-highBounds);
+
+        return arraySelector(chopValue, lowSplit, highSplit);
+    }
+
+    private int[] arraySelector(int chopValue, int[] lowSplit, int[] highSplit)
+    {
+        return lowSplit[^1] <= chopValue ? highSplit : lowSplit;
     }
 }
